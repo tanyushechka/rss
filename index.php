c7de082c7815afc52a0150cd6e8f160779c9c6cc
dtg <tagedo74@gmail.com>
2015-11-10 01:03:33 +0300

    +

--- index.php
+++ index.php
@@ -14,19 +14,6 @@ session_start();
 $_SESSION['access_token'] = '6cd6202680a20796889a537ae28bb51e';
 $_SESSION['access_secret']= '2d2c4ec57fd374c4';
 
-//$client = new HttpClient();
-//$logger = new Logger('name');
-//$fastFeed = new FastFeed($client, $logger);
-//$parser = new RSSParser();
-//$fastFeed->pushParser($parser);
-//$fastFeed->addFeed('upwork', 'https://www.upwork.com/jobs/rss?cn1[]=Web%2C+Mobile+%26+Software+Dev&cn2[]=Web+Development&t[]=0&t[]=1&dur[]=0&dur[]=1&dur[]=13&dur[]=26&dur[]=none&wl[]=10&wl[]=30&wl[]=none&tba[]=0&tba[]=1-9&tba[]=10-&exp[]=1&exp[]=2&exp[]=3&amount[]=Min&amount[]=Max&sortBy=s_ctime+desc&_redirected');
-//$items = $fastFeed->fetch('upwork');
-//foreach ($items as $item) {
-//    var_dump($item);
-//    die;
-//    echo '<p>' . $item->getName() . '</p>' . PHP_EOL;
-//}
-
 $config = new Config(
     [
         'consumerKey'       => '0b890e685dbaa9fddaf51df47f924096',
@@ -34,7 +21,7 @@ $config = new Config(
         'accessToken'       => $_SESSION['access_token'],
         'accessSecret'      => $_SESSION['access_secret'],
 //        'verifySsl'         => true,
-        'debug'             => true,
+        'debug'             => false,
         'authType'          => 'OAuthPHPLib'
     ]
 );
@@ -61,9 +48,32 @@ if (!empty($_SESSION['access_token']) && !empty($_SESSION['access_secret'])) {
 $auth = new Auth($client);
 $info = $auth->getUserInfo();
 
-print_r($info);
+//print_r($info);
 $jobs = new Search($client);
-$params = array("q" => "python", "title" => "Web Developer");
-$jobs->find($params);
+$params = ["title" => "Web"];
+$jj = $jobs->find($params);
+var_dump($jj->jobs);
+//$k = 9;
+//var_dump($jj->jobs[$k]->id);
+//var_dump($jj->jobs[$k]->url);
+//var_dump($jj->jobs[$k]->date_created);
+//var_dump($jj->jobs[$k]->title);
+//var_dump($jj->jobs[$k]->snippet);
+//var_dump($jj->jobs[$k]->job_type);
+//var_dump($jj->jobs[$k]->budget);
+//var_dump($jj->jobs[$k]->duration);
+//var_dump($jj->jobs[$k]->workload);
 
+
+$httpClient = new HttpClient();
+$logger = new Logger('name');
+$fastFeed = new FastFeed($httpClient, $logger);
+$parser = new RSSParser();
+$fastFeed->pushParser($parser);
+$fastFeed->addFeed('upwork', 'https://www.upwork.com/jobs/rss?cn1[]=Web%2C+Mobile+%26+Software+Dev&cn2[]=Web+Development&t[]=0&t[]=1&dur[]=0&dur[]=1&dur[]=13&dur[]=26&dur[]=none&wl[]=10&wl[]=30&wl[]=none&tba[]=0&tba[]=1-9&tba[]=10-&exp[]=1&exp[]=2&exp[]=3&amount[]=Min&amount[]=Max&sortBy=s_ctime+desc&_redirected');
+$items = $fastFeed->fetch('upwork');
+foreach ($items as $key => $item) {
+    var_dump($item);
+
+}
 
